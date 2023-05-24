0c80084aaacc77bb38354057d5749e01aee732cb
GeeksforGeeks <mazmmmdmnb@gmail.com>
2023-05-24 23:12:05 +0300

    CalculMVVM

---
+++ Calculator/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Calculator.Core.Calculators;
+using Calculator.Views;
+using Prism.Ioc;
+using Prism.Unity;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterSingleton<ICalculator, ExpressionCalculator>();
+        }
+
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+    }
+}
---
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Calculator.Core.Calculators;
+using Calculator.Mvvm;
+
+namespace Calculator.ViewModels
+{
+    public class MainWindowViewModel:ViewModelBase
+    {
+        private readonly ICalculator _calculator;
+        public DelegateCommand<object> ButtonPushedCommand { get; set; }
+        public DelegateCommand ClearCommand { get; set; }
+        public DelegateCommand EqualsCommand { get; set; }
+
+        public string Expression
+        {
+            get => GetValue<string>();
+            set => SetValue(value);
+        }
+
+        public string Title { get; set; } = "Cool Calculator";
+
+        public MainWindowViewModel(ICalculator calculator)
+        {
+            _calculator = calculator;
+            ButtonPushedCommand = new DelegateCommand<object>(ButtonPushed);
+            ClearCommand = new DelegateCommand(Clear);
+            EqualsCommand = new DelegateCommand(Equals, EqualsCanExecute);
+        }
+
+        private bool EqualsCanExecute()
+        {
+            return string.IsNullOrWhiteSpace(Expression) == false;
+        }
+
+        private void Equals()
+        {
+            var value = _calculator.Calculate(Expression);
+            Expression = value.ToString();
+        }
+
+        private void Clear()
+        {
+            Expression = null;
+
+            EqualsCommand.RaiseCanExecuteChanged();
+        }
+
+        private void ButtonPushed(object obj)
+        {
+            Expression += obj.ToString();
+            EqualsCommand.RaiseCanExecuteChanged();
+        }
+    }
+}
